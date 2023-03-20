e19d8930e8585493e47dd265eb52f4d1ab87f3ce
lekan474 <olalekanisaiahidowu@gmail.com>
2023-03-20 16:06:57 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
-
-* main - Entry point
-
-*
-
-* Return: Always 0 (Success)
-
-*/
-
+ *  main - initializing main function
+ *  Return: returns 0
+ */
 int main(void)
-
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-        return (0);
-
+	return (0);
 }
